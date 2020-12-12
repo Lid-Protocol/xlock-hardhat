1c28af73d57248069d2044e043bdafe91993a2f1
devstar <dev_star11@outlook.com>
2020-12-12 18:55:58 +1000

    fix deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,14 +5,14 @@ async function main() {
     const Xeth = await ethers.getContractFactory("XETH")
     const Xlocker = await ethers.getContractFactory("XLOCKER")
 
-    const xeth = await upgrades.deployProxy(Xeth, [42]);
+    const xeth = await upgrades.deployProxy(Xeth, [42])
     await xeth.deployed()
     console.log("xeth deployed to:", xeth.address)
 
-    const xlocker = await upgrades.deployProxy(Xlocker, [42], xeth.address)
-    // const xlocker = await Xlocker.deploy(xeth.address)
+    const xlocker = await upgrades.deployProxy(Xlocker, [42])
     await xlocker.deployed()
     console.log("xlocker deployed to:", xlocker.address)
+    xlocker.initialize(xeth.address, "sweepReceiver", "maxXEthWad", "maxTokenWad")
 
     await xeth.transferXlocker(xlocker.address)
     console.log("xlocker permission on xeth transferred")
