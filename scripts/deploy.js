efc39118c9442a98391c64845dcd049777514a62
devstar <dev_star11@outlook.com>
2020-12-15 10:56:20 +1000

    update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,4 @@
+const { BigNumber } = require("ethers")
 const { ethers, upgrades } = require("hardhat")
 
 async function main() {
@@ -5,14 +6,13 @@ async function main() {
     const Xeth = await ethers.getContractFactory("XETH")
     const Xlocker = await ethers.getContractFactory("XLOCKER")
 
-    const xeth = await upgrades.deployProxy(Xeth, [42])
+    const xeth = await Xeth.deploy()
     await xeth.deployed()
     console.log("xeth deployed to:", xeth.address)
 
-    const xlocker = await upgrades.deployProxy(Xlocker, [42])
+    const xlocker = await upgrades.deployProxy(Xlocker, ["0x874Bc0ec6D5d3a21c926067E29299360a7829d07", "0xb63c4F8eCBd1ab926Ed9Cb90c936dffC0eb02cE2", BigNumber.from("1000000000000000000000000"), BigNumber.from("1000000000000000000000000000000")])
     await xlocker.deployed()
     console.log("xlocker deployed to:", xlocker.address)
-    xlocker.initialize(xeth.address, "sweepReceiver", "maxXEthWad", "maxTokenWad")
 
     await xeth.transferXlocker(xlocker.address)
     console.log("xlocker permission on xeth transferred")
