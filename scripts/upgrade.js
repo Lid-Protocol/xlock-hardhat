5d608560dec2b9daac086aa43b53786bede08599
carlsbad <carlsbad_sunshine@protonmail.com>
2021-01-21 19:07:39 -0800

    Upgrade xlocker

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -2,8 +2,9 @@ const { ethers, upgrades } = require("hardhat");
 const XlockerAddress = "0xAA13f1Fc73baB751Da08930007D4D847EeEafAA2";
 
 async function main() {
-    const Xeth = await ethers.getContractFactory("XETH")
-    const Xlocker = await upgrades.upgradeProxy(XlockerAddress, Xlocker);
+    console.log("Upgrading xlocker..")
+    const XLOCKER = await ethers.getContractFactory("XLOCKER")
+    const Xlocker = await upgrades.upgradeProxy(XlockerAddress, XLOCKER);
     console.log("xlocker upgraded");
   }
   
